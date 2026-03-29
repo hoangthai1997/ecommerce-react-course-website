4e1dc9f0190fbbffcb5727a4fe439c8111c6311e
Vương Quốc Hoàng Thái <vuongquochoangthai@gmail.com>
2026-03-29 19:59:32 +0700

    Auth SignUp

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,19 @@
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext(null);
+
+export default function AuthProvider({children}) {
+    const [user, setUser] = useState(null);
+
+    function signUp(email, password) {
+        const users = [];
+
+        const newUser = {email, password};
+        users.push(newUser);
+        localStorage.setItem("users", JSON.stringify(users));
+    }
+
+    function login() {}
+
+    return <AuthContext.Provider value={{signUp}}>{children}</AuthContext.Provider>
+}
