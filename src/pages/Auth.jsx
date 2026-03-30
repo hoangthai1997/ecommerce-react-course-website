147f0810d08cfd8f44fd5f48620d83bc80a9b3cf
Vương Quốc Hoàng Thái <vuongquochoangthai@gmail.com>
2026-03-30 19:27:31 +0700

    Added auth

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,24 +1,44 @@
 import { useContext, useState } from "react";
 import { useForm } from "react-hook-form";
 import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function Auth() {
     const [mode, setMode] = useState("signup");
-    const {signUp} = useContext(AuthContext);
+    const [error, setError] = useState(null);
+
+    const navigate = useNavigate();
+
+    const {signUp, user, logout, login} = useContext(AuthContext);
 
     const { register, handleSubmit, formState: { errors } } = useForm();
 
     function onSubmit(data) {
-        signUp(data.email, data.password);
+        setError(null);
+        let result;
+        if (mode === "signup") {
+            result = signUp(data.email, data.password);
+        } else {
+            result = login(data.email, data.password);
+        }
+        
+        if (result.success) {
+            navigate("/");
+        } else {
+            setError(result.error);
+        }
     }
 
     return <div className="page">
         <div className="container">
             <div className="auth-container">
+                {user && <p>User logged in: {user.email}</p>}
+                <button onClick={() => logout()}>Logout</button>
                 <h1 className="page-title">
                     {mode === "signup" ? "Sign Up" : "Login"}
                 </h1>
                 <form className="auth-form" onSubmit={handleSubmit(onSubmit)}>
+                    {error && <div className="error-message">{error}</div>}
                     <div className="form-group">
                         <label className="form-label" htmlFor="email">Email</label>
                         <input className="form-input" type="email" id="email" {...register('email', { required: "Email is required" })} />
