909f80168f1e52c6fd91f2c9a78353ccda5f7262
Vương Quốc Hoàng Thái <vuongquochoangthai@gmail.com>
2026-03-31 19:11:37 +0700

    ProductDetails

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,48 +1,60 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useContext } from "react";
 
-export const AuthContext = createContext(null);
+const AuthContext = createContext(null);
 
-export default function AuthProvider({children}) {
-    const [user, setUser] = useState(localStorage.getItem("currentUserEmail") ? {email: localStorage.getItem("currentUserEmail")} : null);
+export default function AuthProvider({ children }) {
+  const [user, setUser] = useState(
+    localStorage.getItem("currentUserEmail")
+      ? { email: localStorage.getItem("currentUserEmail") }
+      : null
+  );
 
-    function signUp(email, password) {
-        const users = JSON.parse(localStorage.getItem('users') || "[]");
+  function signUp(email, password) {
+    const users = JSON.parse(localStorage.getItem("users") || "[]");
 
-        if (users.find(u => u.email === email)) {
-            return {
-                success: false,
-                error: "Email already exists",
-            };
-        }
+    if (users.find((u) => u.email === email)) {
+      return { success: false, error: "Email already exists" };
+    }
+    const newUser = { email, password };
+    users.push(newUser);
+    localStorage.setItem("users", JSON.stringify(users));
+    localStorage.setItem("currentUserEmail", email);
 
-        const newUser = {email, password};
-        users.push(newUser);
-        localStorage.setItem("users", JSON.stringify(users));
-        localStorage.setItem("currentUserEmail", email);
+    setUser({ email });
 
-        setUser({email});
+    return { success: true };
+  }
 
-        return {success: true};
+  function login(email, password) {
+    const users = JSON.parse(localStorage.getItem("users") || "[]");
+    const user = users.find(
+      (u) => u.email === email && u.password === password
+    );
+
+    if (!user) {
+      return { success: false, error: "Invalid email or password" };
     }
 
-    function login(email, password) {
-        const users = JSON.parse(localStorage.getItem('users') || "[]");
-        const user = users.find((u) => u.email === email && u.password === password);
+    localStorage.setItem("currentUserEmail", email);
+    setUser({ email });
 
-        if (!user) {
-            return {success: false, error: "Invalid email or password"};
-        }
+    return { success: true };
+  }
 
-        localStorage.setItem("currentUserEmail", email);
-        setUser({user});
+  function logout() {
+    localStorage.removeItem("currentUserEmail");
+    setUser(null);
+  }
 
-        return {success: true};
-    }
+  return (
+    <AuthContext.Provider value={{ signUp, user, logout, login }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
 
-    function logout() {
-        localStorage.removeItem("currentUserEmail");
-        setUser(null);
-    }
+export function useAuth() {
+  const context = useContext(AuthContext);
 
-    return <AuthContext.Provider value={{signUp, user, logout, login}}>{children}</AuthContext.Provider>
+  return context;
 }
